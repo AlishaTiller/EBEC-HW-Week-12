0fb47be0a8dc6849b05e74e1d66887db6cc0c201
Alisha Tiller <abtiller@purdue.edu>
2021-11-22 21:36:33 -0500

    completely finished program!

--- wof_anaylsis_abtiller.py
+++ wof_anaylsis_abtiller.py
@@ -1,13 +1,17 @@
 """
-Author: Your Name, login@purdue.edu
-Assignment: m.n - Assignment Name
-Date: MM/DD/YYYY
+Author: Alisha Tiller, abtiller@purdue.edu
+Assignment: 12.2 - Wof Analysis
+Date: 11/22/2021
 
 Description:
-    Describe your program here.
+    The program counts the number of times each letter is used from 
+    the phrases that could be used in a game of Wheel of Fortune. It 
+    then takes this data and creates a bar graph showing the occurrences 
+    of each letter relative to the total number of letters in phrase.txt
+    file.
 
 Contributors:
-    Name, login@purdue.edu [repeat for each]
+    None
 
 My contributor(s) helped me:
     [ ] understand the assignment expectations without
@@ -30,58 +34,51 @@ import matplotlib.pyplot as plt
 
 def main():
 
-    # start an empty list for the gas prices
     letters = []
     frequency_of_letter = {}
     with open('Phrases.txt', 'r') as phrases:
         lines = phrases.readlines()
-        # for each line in the file, add that value(number) to the avg_gas_price to form a list
+        # for each line in the file, delete the space, add to a list
         for line in lines: 
             line = line.strip()
             letters.append(line)
-            x = "".join(letters)
-            x = x.replace('-', ' ')
-            x = x.replace("'", ' ')
-            x = x.replace(' ', '')
-            x = x.upper()
-        
-        # print(f'{x}')
-
-        for letter in x:
+            # turn many strings into one string
+            phrases_in_one_str = "".join(letters)
+            # replace the following with a space or nothing
+            phrases_in_one_str = phrases_in_one_str.replace('-', ' ')
+            phrases_in_one_str = phrases_in_one_str.replace("'", ' ')
+            phrases_in_one_str = phrases_in_one_str.replace(' ', '')
+            phrases_in_one_str = phrases_in_one_str.upper()
+        # for each letter in phrases_in_one_string... 
+        for letter in phrases_in_one_str:
+            # if the letter is in the list frequency_of_letter, add an occurrence to that letter
             if letter in frequency_of_letter:
                 frequency_of_letter[letter] += 1
             else:
                 frequency_of_letter[letter] = 1
-
-        # print(f'{frequency_of_letter}')
-    
+        # x-values/labels for graph and key to access dicitonary
         alphabet = ['A','B','C','D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 
                     'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'] 
+        # to create a list of elements for the y-values of the graph
         index = 0
         quantity_of_letter = []
-        for z in alphabet:
-            occurrences = alphabet[index]
-            y = frequency_of_letter[occurrences]
-            y = y/915
-            quantity_of_letter.append(y)
+        for single_letter in alphabet:
+            # to access each element in a list one at a time for all 26 letters in alphabet
+            single_letter = alphabet[index]
+            # use the element as the key to access the value in the frequency_of_letter dictionary
+            occurrences = frequency_of_letter[single_letter]
+            # the number of times each letter is used divided by the total number of letters
+            occurrences = occurrences/915
+            # add this occurrence value to a new list 
+            quantity_of_letter.append(occurrences)
             index += 1
-        
-        # print(f'{quantity_of_letter}')
+        # totals the number of occurences of each letter from the list (= 915)
         total = sum(quantity_of_letter)
-        # print(f'{total}')
-
-        key = 0 
-        x_values = []
-        for k in alphabet:
-            x_keys = alphabet[key]
-            x_values.append(x_keys)
-            key += 1
-
     # details for creating a line graph 
     fig, ax = plt.subplots()
-    x = x_values
-    y = quantity_of_letter
-    ax.bar(x,y)
+    phrases_in_one_str = alphabet
+    occurrences = quantity_of_letter
+    ax.bar(phrases_in_one_str,occurrences)
     ax.set_title('Letter Frequency in Puzzle Phrases')
     ax.set_xlabel('Letter')
     ax.set_ylabel('Letter Apperance Frequency')
