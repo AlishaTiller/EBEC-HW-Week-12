093b163d10b58f441c6da9b153317781ec5839c6
Alisha Tiller <abtiller@purdue.edu>
2021-12-07 21:55:03 -0500

    fixed errors

--- solo_wof_abtiller.py
+++ solo_wof_abtiller.py
@@ -89,13 +89,16 @@ def status_report(round, phrase, guessed, round_earnings):
 
 def spin_the_wheel_action(phrase, guessed, player_current_money):
     remaining_consonants = list(filter(lambda letter: not letter in guessed, consonants))
+    if len(remaining_consonants) <= 0:
+        print('There are no more consonants to choose.')
+        return '', 0
     # player is asked to choose a consonant from the list of unused consonants
     # call the spin_the_wheel function which returns a value at random from a virtual wheel
     cash_value = spin_the_wheel()
     # if bankrupt choosem the player's cash earnings for the round are set to zero and the turn ends
     if cash_value == 'BANKRUPT':
         print(f'The wheel landed on BANKRUPT.\nYou lost ${player_current_money:,}!')
-        return '', - player_current_money, True
+        return '', - player_current_money
     print(f'The wheel landed on ${cash_value:,}.')
     while True:
         player_choice = input('Pick a consonant: ')
@@ -123,16 +126,16 @@ def spin_the_wheel_action(phrase, guessed, player_current_money):
     # check to see if what the player entered is a string of characters
     if not player_choice in phrase:
         print(f"I'm sorry, there are no {player_choice}'s.")
-        return player_choice, 0, False
+        return player_choice, 0
     # count the number of times that letter appears in the phrase 
     occurrences_of_letter = phrase.count(f'{player_choice}')
     dollar_amount_earned = cash_value * occurrences_of_letter
     if occurrences_of_letter > 1:
         print(f"There are {occurrences_of_letter} {player_choice}'s, which earns you ${dollar_amount_earned:,}.")
     else:
-        print(f'There is {occurrences_of_letter} {player_choice}, which earns you ${dollar_amount_earned:,}.')
+        print(f"There is {occurrences_of_letter} {player_choice}, which earns you ${dollar_amount_earned:,}.")
     # remove the consonant from the consonants_to_choose list and add it to the consonants_used list
-    return player_choice, dollar_amount_earned, True
+    return player_choice, dollar_amount_earned
 
                  
 def vowel_action(phrase, guessed, player_current_money):
@@ -204,17 +207,15 @@ def main():
         while True:
             if round > 4:
                 print(f'You earned a total of ${total_earnings}.')
-                break
+                return
             # index increases by one, so each subsequent round of the game uses the next phrase in the list
             status_report(round, phrase, guessed, round_earnings)
             player_chooses = input('Enter the number of your choice: ')
             # using the player_chooses as the key to access the dictionary which contains the value of what acitvity to do
             if player_chooses == '1':
-                guessed_consonant, dollar_amount_earned, next_round = spin_the_wheel_action(phrase, guessed, round_earnings)
+                guessed_consonant, dollar_amount_earned = spin_the_wheel_action(phrase, guessed, round_earnings)
                 guessed += guessed_consonant
                 round_earnings += dollar_amount_earned
-                # if next_round:
-                #     round += 1 
             elif player_chooses == '2':
                 guessed_vowel = vowel_action(phrase, guessed, round_earnings)
                 guessed += guessed_vowel
